304eee88f9951c00e2922b8d08cd5f51df00d77e
Liz-Muchiri <leezmuchiri@gmail.com>
2023-07-19 14:41:05 -0700

    simple UNIX command line interpreter that can handle command lines with arguments

---
+++ Comargs.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAX_COMMAND_LENGTH 100
+#define MAX_ARGUMENTS 10
+
+int main() {
+    char command[MAX_COMMAND_LENGTH];
+    char prompt[] = "simple_shell> ";
+
+    while (1) {
+        printf("%s", prompt);
+
+        if (fgets(command, sizeof(command), stdin) == NULL) {
+            printf("\n");
+            break;
+        }
+
+        // Remove the newline character at the end of the command
+        command[strcspn(command, "\n")] = '\0';
+
+        // Tokenize the command into arguments
+        char* arguments[MAX_ARGUMENTS];
+        char* token = strtok(command, " ");
+        int argCount = 0;
+
+        while (token != NULL && argCount < MAX_ARGUMENTS - 1) {
+            arguments[argCount++] = token;
+            token = strtok(NULL, " ");
+        }
+
+        arguments[argCount] = NULL;
+
+        // Fork a child process
+        pid_t pid = fork();
+
+        if (pid < 0) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            // Child process
+
+            // Execute the command
+            if (execvp(arguments[0], arguments) == -1) {
+                perror("execvp");
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            // Parent process
+
+            // Wait for the child process to finish
+            int status;
+            waitpid(pid, &status, 0);
+        }
+    }
+
+    return 0;
+}
+
