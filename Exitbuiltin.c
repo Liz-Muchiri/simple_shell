314ed92202869ee481584c3ca788c356b55f7bdb
Vicky-E <victoriaemmanuel630@gmail.com>
2023-07-25 04:27:11 -0700

    fixed all betty errors

--- Exitbuiltin.c
+++ Exitbuiltin.c
@@ -1,81 +1,74 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include "shell.h"
 
 #define MAX_COMMAND_LENGTH 100
 #define MAX_ARGUMENTS 10
 
-int main() {
-    char command[MAX_COMMAND_LENGTH];
-    char prompt[] = "simple_shell> ";
+int main(void)
+{
+	char *command = NULL;
+	size_t command_size = 0;
+	char prompt[] = "simple_shell> ";
+	int status, argCount;
 
-    while (1) {
-        printf("%s", prompt);
+	while (1)
+	{
+		printf("%s", prompt);
 
-        if (fgets(command, sizeof(command), stdin) == NULL) {
-            printf("\n");
-            break;
-        }
+		if (getline(&command, &command_size, stdin) == -1)
+		{
+			printf("\n");
+			free(command);
+			break;
+		}
+		command[strcspn(command, "\n")] = '\0';
+		char *arguments[MAX_ARGUMENTS];
+		char *token = strtok(command, " ");
 
-        // Remove the newline character at the end of the command
-        command[strcspn(command, "\n")] = '\0';
+		argCount = 0;
 
-        // Tokenize the command into arguments
-        char* arguments[MAX_ARGUMENTS];
-        char* token = strtok(command, " ");
-        int argCount = 0;
+		while (token != NULL && argCount < MAX_ARGUMENTS - 1)
+		{
+			arguments[argCount++] = token;
+			token = strtok(NULL, " ");
+		}
 
-        while (token != NULL && argCount < MAX_ARGUMENTS - 1) {
-            arguments[argCount++] = token;
-            token = strtok(NULL, " ");
-        }
+		arguments[argCount] = NULL;
+		if (strcmp(arguments[0], "exit") == 0)
+			break;
+		char *path = getenv("PATH");
+		char *pathToken = strtok(path, ":");
 
-        arguments[argCount] = NULL;
+		while (pathToken != NULL)
+		{
+			char commandPath[MAX_COMMAND_LENGTH];
 
-        // Check if the command is the exit built-in
-        if (strcmp(arguments[0], "exit") == 0) {
-            break;
-        }
+			snprintf(commandPath, sizeof(commandPath),
+					"%s/%s", pathToken, arguments[0]);
 
-        // Check if the command exists in the PATH
-        char* path = getenv("PATH");
-        char* pathToken = strtok(path, ":");
+			if (access(commandPath, X_OK) == 0)
+			{
+				pid_t pid = fork();
 
-        while (pathToken != NULL) {
-            char commandPath[MAX_COMMAND_LENGTH];
-            snprintf(commandPath, sizeof(commandPath), "%s/%s", pathToken, arguments[0]);
+				if (pid < 0)
+				{
+					perror("fork");
+					exit(EXIT_FAILURE);
+				}
+				else if (pid == 0)
+				{
+					if (execv(commandPath, arguments) == -1)
+					{
+						perror("execv");
+						exit(EXIT_FAILURE);
+					}
+				}
+				else
+					waitpid(pid, &status, 0);
+				break;
+			}
 
-            if (access(commandPath, X_OK) == 0) {
-                // Command exists, fork a child process
-                pid_t pid = fork();
-
-                if (pid < 0) {
-                    perror("fork");
-                    exit(EXIT_FAILURE);
-                } else if (pid == 0) {
-                    // Child process
-
-                    // Execute the command
-                    if (execv(commandPath, arguments) == -1) {
-                        perror("execv");
-                        exit(EXIT_FAILURE);
-                    }
-                } else {
-                    // Parent process
-
-                    // Wait for the child process to finish
-                    int status;
-                    waitpid(pid, &status, 0);
-                }
-
-                break;
-            }
-
-            pathToken = strtok(NULL, ":");
-        }
-    }
-
-    return 0;
+			pathToken = strtok(NULL, ":");
+		}
+	}
+	return (0);
 }
-
